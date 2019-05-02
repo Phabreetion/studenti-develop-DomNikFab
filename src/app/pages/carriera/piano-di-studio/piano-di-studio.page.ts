9bd9648948da5a6591be18235c80d17a870e046d
RedHitMark <russodivito.marco@gmail.com>
2019-05-02 21:27:23 +0200

    Creazione nuova pagina per piano di studio.

---
+++ src/app/pages/carriera/piano-di-studio/piano-di-studio.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-piano-di-studio',
+  templateUrl: './piano-di-studio.page.html',
+  styleUrls: ['./piano-di-studio.page.scss'],
+})
+export class PianoDiStudioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
