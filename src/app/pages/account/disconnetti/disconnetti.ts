1881f8b6f7229421dafe0aa41a741dea3282aa1d
Fausto Fasano <fausto.fasano@unimol.it>
2019-05-04 17:34:02 +0200

    Bug-Fix disconnessione. Dopo la disconnessione erano ancora visibili i menù associato all ruolo.

--- src/app/pages/account/disconnetti/disconnetti.ts
+++ src/app/pages/account/disconnetti/disconnetti.ts
@@ -88,6 +88,7 @@ export class DisconnettiPage implements OnInit {
                             // Conserviamo il valore dell'URL del backend in caso di riavvio dell'app
                             this.storage.set('baseurl', this.globalData.baseurl);
                             this.globalData.logged = false;
+                            this.globalData.userRole = '';
                             this.globalData.goTo(this.globalData.srcPage, '/login', 'root', false);
                             this.toastCtrl.create({
                                 message: response.toString(),
@@ -141,13 +142,9 @@ export class DisconnettiPage implements OnInit {
                         // Conserviamo il valore dell'URL del backend in caso di riavvio dell'app
                         this.storage.set('baseurl', this.globalData.baseurl);
                         this.globalData.logged = false;
-
-                        this.notificheService.rimuoviSottoscrizioni().then(
-                            () => {
-                                this.globalData.goTo('/login', '/login', 'root', false);
-
-                            }
-                        );
+                        this.globalData.userRole = '';
+                        this.notificheService.rimuoviSottoscrizioni();
+                        this.globalData.goTo('/login', '/login', 'root', false);
                     }
                 },
                 {
