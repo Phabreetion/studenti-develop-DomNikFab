9cacb1ea3b1ddce936bca9cff3a9bbb483011db0
RedHitMark <russodivito.marco@gmail.com>
2019-05-04 14:37:58 +0200

    Aggiunto ordinamento per cfu

--- src/app/pages/carriera/piano-di-studio/piano-di-studio.page.ts
+++ src/app/pages/carriera/piano-di-studio/piano-di-studio.page.ts
@@ -6,6 +6,8 @@ const VOTO_CRESCENTE: number = 3;
 const VOTO_DECRESCENTE: number = 4;
 const ANNO_CRESCENTE: number = 5;
 const ANNO_DECRESCENTE: number = 6;
+const CFU_CRESCENTE: number = 7;
+const CFU_DECRESCENTE: number = 8;
 
 @Component({
   selector: 'app-piano-di-studio',
@@ -27,14 +29,14 @@ export class PianoDiStudioPage implements OnInit {
 
   ngOnInit() {
     this.corsi = [
-      {id: 1, anno: 1, nome: 'Programmazione', voto:30},
-      {id: 2, anno: 1, nome: 'Matematica', voto: 20},
-      {id: 3, anno: 1, nome: 'Informatica giuridica', voto: 20},
-      {id: 4, anno: 2, nome: 'Storia della matematica', voto: 18},
-      {id: 5, anno: 2, nome: 'Ingegneria del software', voto: 27},
-      {id: 6, anno: 2, nome: 'Basi di dati e sistemi informativi', voto: 28},
-      {id: 7, anno: 3, nome: 'Informatica territoriale'},
-      {id: 8, anno: 3, nome: 'Ricerca operativa'},
+      {id: 1, anno: 1, nome: 'Programmazione', cfu: 12, voto:30},
+      {id: 2, anno: 1, nome: 'Matematica', cfu: 12, voto: 20},
+      {id: 3, anno: 1, nome: 'Informatica giuridica', cfu: 6,voto: 20},
+      {id: 4, anno: 2, nome: 'Storia della matematica', cfu: 6,voto: 18},
+      {id: 5, anno: 2, nome: 'Ingegneria del software', cfu: 9,voto: 27},
+      {id: 6, anno: 2, nome: 'Basi di dati e sistemi informativi', cfu: 12,voto: 28},
+      {id: 7, anno: 3, nome: 'Informatica territoriale', cfu: 8,},
+      {id: 8, anno: 3, nome: 'Ricerca operativa', cfu: 7,},
     ]; // Prova per testare
     this.filtra();
   }
@@ -76,6 +78,16 @@ export class PianoDiStudioPage implements OnInit {
       case ANNO_DECRESCENTE: //anno decrescente
         this.corsi.sort((one, two) => (parseInt(one.anno) > parseInt(two.anno) ? -1 : 1));
         break;
+
+      case CFU_CRESCENTE: //anno crescente
+        this.corsi.sort((one, two) => (parseInt(one.cfu) < parseInt(two.cfu) ? -1 : 1));
+        break;
+
+      case CFU_DECRESCENTE: //anno decrescente
+        this.corsi.sort((one, two) => (parseInt(one.cfu) > parseInt(two.cfu) ? -1 : 1));
+        break;
+
+
 
     }
 
