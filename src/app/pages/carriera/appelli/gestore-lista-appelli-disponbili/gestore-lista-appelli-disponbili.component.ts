27df10501f37fec641d4579397307cdbe21fec59
Marco <russodivito.marco@gmail.com>
2019-05-07 16:20:11 +0200

    Creazione componente per la gestione degli appelli disponibili

---
+++ src/app/pages/carriera/appelli/gestore-lista-appelli-disponbili/gestore-lista-appelli-disponbili.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestore-lista-appelli-disponbili',
+  templateUrl: './gestore-lista-appelli-disponbili.component.html',
+  styleUrls: ['./gestore-lista-appelli-disponbili.component.scss'],
+})
+export class GestoreListaAppelliDisponbiliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
