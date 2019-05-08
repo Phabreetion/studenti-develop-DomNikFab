0f018ef3c4de9f3b2d1d187decb754d7e3828916
antoniodeturris0 <antoniodeturris0@gmail.com>
2019-05-08 16:14:58 +0200

    Definite classi Appello, AppelloDisponibile,AppelloPrenotato

--- src/app/models/Appello.ts
+++ src/app/models/Appello.ts
@@ -1,3 +1,25 @@
 export class Appello {
-    //@TODO
+
+
+    public NOME: string; // nome appello
+
+    public DATA: string; // data in cui si svolge l'appello
+
+    public ORARIO: string; // ora dell'appello
+
+    public TIPOLOGIA: string; // scritto/orale
+
+    public CODICE_ESAME: number; // codice esame
+
+    public ISCRITTI_APPELLO: number; // iscritti all'appello
+
+
+    constructor(NOME?: string, DATA?: string, ORARIO?: string, TIPOLOGIA?: string, CODICE_ESAME?: number, ISCRITTI_APPELLO?: number) {
+        this.NOME = NOME;
+        this.DATA = DATA;
+        this.ORARIO = ORARIO;
+        this.TIPOLOGIA = TIPOLOGIA;
+        this.CODICE_ESAME = CODICE_ESAME;
+        this.ISCRITTI_APPELLO = ISCRITTI_APPELLO;
+    }
 }
