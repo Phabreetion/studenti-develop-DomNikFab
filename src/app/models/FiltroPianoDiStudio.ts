c9c61bdc05706b0ee9ed82c9173c84f4bcd269d9
RedHitMark <russodivito.marco@gmail.com>
2019-05-15 22:34:55 +0200

    Miglioramenti nella gestione dei filtri

--- src/app/models/FiltroPianoDiStudio.ts
+++ src/app/models/FiltroPianoDiStudio.ts
@@ -1,24 +1,36 @@
 export class FiltroPianoDiStudio {
-    public filtroSuperatiAttivo: boolean;
-    public filtroNonSuperatiAttivo: boolean;
-    public filtroPerAnno: number; //-1 non attivo -> altrimenti gli altri
-    public idOrdinamento: number;
-    public tipoOrdinamento: number; //0 crescente --- 1 decrescente
+
+    filtroSuperatiAttivo: boolean;
+    filtroNonSuperatiAttivo: boolean;
+    filtroPerAnno: number; //0 non attivo -> altrimenti gli altri
+    idOrdinamento: number;
+    tipoOrdinamento: number; //0 crescente --- 1 decrescente
 
 
+    constructor() {
+        //default value
+        this.reset();
+    }
+
+
+    static toObj(obj: Object): FiltroPianoDiStudio {
+        return Object.assign(new FiltroPianoDiStudio(), obj);
+    }
 
     isActive(): boolean {
         return this.filtroSuperatiAttivo || this.filtroNonSuperatiAttivo || this.filtroPerAnno > 0;
+    }
+
+    reset() {
+        this.filtroSuperatiAttivo = false;
+        this.filtroNonSuperatiAttivo = false;
+        this.filtroPerAnno = 0;
+        this.idOrdinamento = 0;
+        this.tipoOrdinamento = 0;
     }
 
     disableBothFiltri(): void {
         this.filtroNonSuperatiAttivo = false;
         this.filtroSuperatiAttivo = false;
     }//rivedere
-
-
-
-    static toObj(obj: Object): FiltroPianoDiStudio {
-        return Object.assign(new FiltroPianoDiStudio(), obj);
-    }
 }
